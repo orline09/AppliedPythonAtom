387eec5d3e57a58edf5d1eb2c89fa6636fc929ca
orline09 <littlehappiness4@gmail.com>
2019-03-06 07:49:16 +0300

    try calculate, dont test 001

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -10,4 +10,14 @@ def calculator(x, y, operator):
     :param operator: 4 оператора: plus, minus, mult, divide
     :return: результат операции или None, если операция не выполнима
     '''
+    if operator == 'plus':
+        return x + y
+    elif operator == 'minus':
+        return x - y
+    elif operator == 'mult':
+        return x * y
+    elif operator == 'divide' and y != 0:
+        return x / y
+    else:
+        return None
     raise NotImplementedError
