7ce149800a9769644a7adb0c8d0fdb36bd5b05ad
orline09 <littlehappiness4@gmail.com>
2019-03-07 01:21:16 +0300

    fix1.2

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,6 +2,26 @@
 # coding: utf-8
 
 
+def det(matr, mult):
+    N = len(matr)
+    if N == 1:
+        return mult * matr[0][0]
+    else:
+        s = -1
+        res = 0
+        for i in range(N):
+            matr_ostatok = []
+            for j in range(1, N):
+                buff = []
+                for k in range(N):
+                    if k != i:
+                        buff.append(matr[j][k])
+                matr_ostatok.append(buff)
+            s *= -1
+            res += mult * det(matr_ostatok, s * matr[0][i])
+        return res
+
+
 def calculate_determinant(list_of_lists):
     '''
     Метод, считающий детерминант входной матрицы,
@@ -11,36 +31,10 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    a = list_of_lists
-    if a is None:
+    for i in range(len(list_of_lists)):
+        if len(list_of_lists) != len(list_of_lists[0]):
+            return None
+    if list_of_lists is None:
         return None
-    if len(a) == 1 and len(a[0]) == 1:
-        return a[0][0]
-    if len(a) != len(a[0]):
-        return None
-    if len(a) == 2:
-        sum = a[0][0] * a[1][1] - a[0][1] * a[1][0]
-        return sum
-    sum = 0
-    for i in range(len(a)):
-        mul = 1
-        k = i
-        for j in range(len(a)):
-            mul *= a[j][k]
-            if (k == len(a) - 1):
-                k = 0
-            else:
-                k = k + 1
-        sum += mul
-    for i in range(len(a)):
-        mul = 1
-        k = i
-        for j in range(len(a)):
-            mul *= a[j][k]
-            if k == 0:
-                k = len(a) - 1
-            else:
-                k = k - 1
-        sum -= mul
-    return sum
+    return det(list_of_lists, 1)
     raise NotImplementedError
