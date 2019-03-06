d00e938f5741ce01e6464adb875387742c831d99
orline09 <littlehappiness4@gmail.com>
2019-03-06 05:07:46 +0300

    try004

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -9,4 +9,12 @@ def reverse(number):
     :param number: исходное число
     :return: инвертированное число
     '''
+    n2 = 0
+    n1 = number
+    while n1 > 0:
+        digit = n1 % 10 
+        n1 = n1 // 10 #fgdk.j
+        n2 = n2 * 10 
+        n2 = n2 + digit 
+    return n2
     raise NotImplementedError
