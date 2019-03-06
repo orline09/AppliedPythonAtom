e381e9cd49c6394e2b4f15e3f238e969181b6fa1
orline09 <littlehappiness4@gmail.com>
2019-03-06 13:30:18 +0300

    fix1

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 
-def is_bracket_correct(input_string):
+def is_bracket_correct1(input_string):
     '''
     Метод проверяющий является ли поданная скобочная
      последовательность правильной (скобки открываются и закрываются)
@@ -10,26 +10,13 @@ def is_bracket_correct(input_string):
     :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
     :return: True or False
     '''
-    s1 = 0
-    s2 = 0
-    s3 = 0
-    for i in range(len(input_string)):
-        if input_string[i] == '(':
-            s1 += 1
-        elif input_string[i] == '[':
-            s2 += 1
-        elif input_string[i] == '{':
-            s3 += 1
-        if input_string[i] == ')':
-            s1 -= 1
-        elif input_string[i] == ']':
-            s2 -= 1
-        elif input_string[i] == '}':
-            s3 -= 1
-        if s1 < 0 or s2 < 0 or s3 < 0:
-            return False
-    if s1 == s2 == s3:
-        return True
+    text = input_string
+    while '()' in text or '[]' in text or '{}' in text:
+        text = text.replace('()', '')
+        text = text.replace('[]', '')
+        text = text.replace('{}', '')
+    if (text == ''):
+            return True
     else:
-        return False
+            return False
     raise NotImplementedError
