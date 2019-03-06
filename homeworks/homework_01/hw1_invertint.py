8a1ced36221c6c8f9c1829ded080de5193400caa
orline09 <littlehappiness4@gmail.com>
2019-03-06 06:01:15 +0300

    try05

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -3,18 +3,19 @@
 
 
 def reverse(number):
-    '''
-    Метод, принимающий на вход int и
-    возвращающий инвертированный int
-    :param number: исходное число
-    :return: инвертированное число
-    '''
     n2 = 0
     n1 = number
+    k = 0
+    if number < 0:
+        k = 1
+        n1 = abs(n1)
     while n1 > 0:
         digit = n1 % 10 
-        n1 = n1 // 10 #fgdk.j
+        n1 = n1 // 10 
         n2 = n2 * 10 
         n2 = n2 + digit 
-    return n2
+    if k == 1:
+        return -n2
+    else:
+        return n2
     raise NotImplementedError
