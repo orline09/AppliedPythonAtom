397c2abd5509760432f7126ea0f86b0de708c528
orline09 <littlehappiness4@gmail.com>
2019-03-06 16:43:44 +0300

    fix1.2

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,21 +11,36 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    res = 1
     a = list_of_lists
+    if a is None:
+        return None
+    if len(a) == 1 and len(a[0]) == 1:
+        return a[0][0]
     if len(a) != len(a[0]):
-        return False
-    n = len(a)
-    for i in range(n):
-        j = max(range(i, n), key=lambda kk: abs(a[k][i]))
-        if i != j:
-            a[i], a[j] = a[j], a[i]
-            res *= -1
-        if a[i][i] == 0:
-            return 0
-        res *= a[i][i]
-        for j in range(i+1, n):
-            b = a[j][i] / a[i][i]
-            a[j] = [a[j][kk]-b*a[i][kk] for k in range(n)]
-    return res
+        return None
+    if len(a) == 2:
+        sum = a[0][0] * a[1][1] - a[0][1] * a[1][0]
+        return sum
+    sum = 0
+    for i in range(len(a)):
+        mul = 1
+        k = i
+        for j in range(len(a)):
+            mul *= a[j][k]
+            if (k == len(a) - 1):
+                k = 0
+            else:
+                k = k + 1
+        sum += mul
+    for i in range(len(a)):
+        mul = 1
+        k = i
+        for j in range(len(a)):
+            mul *= a[j][k]
+            if k == 0:
+                k = len(a) - 1
+            else:
+                k = k - 1
+        sum -= mul
+    return sum
     raise NotImplementedError
