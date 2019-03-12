84fe5bc187db49b53b52cca401b3ce68715609db
orline09 <littlehappiness4@gmail.com>
2019-03-12 20:56:16 +0300

    add fastmerger, try to work

--- homeworks/homework_02/fastmerger.py
+++ homeworks/homework_02/fastmerger.py
@@ -12,5 +12,17 @@ class FastSortedListMerger:
         принимает на вход список отсортированных непоубыванию списков и число
         на выходе выдает один список длинной k, отсортированных по убыванию
         '''
-
-        raise NotImplementedError
+        res = []
+        i = 0
+        additional = []
+        for i in range(len(list_of_lists)):
+            if list_of_lists[i]:
+                additional.append((list_of_lists[i][-1], i))
+        heap = MaxHeap(additional)
+        for i in range(k):
+            n = heap.extract_maximum()
+            res.append(n[0])
+            list_of_lists[n[1]].pop()
+            if list_of_lists[n[1]]:
+                heap.add((list_of_lists[n[1]][-1], n[1]))
+        return res
