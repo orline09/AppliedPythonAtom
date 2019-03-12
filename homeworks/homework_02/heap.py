b812353473f91e12b08184a8233477b80e0f11b8
orline09 <littlehappiness4@gmail.com>
2019-03-12 20:31:49 +0300

    add heap and rewrite vkposter

--- homeworks/homework_02/heap.py
+++ homeworks/homework_02/heap.py
@@ -5,22 +5,67 @@
 class Heap():
 
     def __init__(self, array):
-        pass
+        self.main = []
+        self.input_data = array[:]
+        self.build_heap()
 
     def add(self, elem_with_priority):
-        pass
+        self.main.append(elem_with_priority)
+        i = len(self.main) - 1
+        while i > 0:
+            parent = (i - 1) // 2
+            if comparator_d(self.main[i], self.main[parent]):
+                leaf = self.main[i]
+                self.main[i] = self.main[parent]
+                self.main[parent] = leaf
+                i = parent
+            else:
+                break
 
     def build_heap(self):
-        pass
+        i = 1
+        if len(self.input_data):
+            self.main.append(self.input_data[0])
+            while i < len(self.input_data):
+                self.add(self.input_data[i])
+                i += 1
 
 
 class MaxHeap(Heap):
 
     def __init__(self, array):
-        raise NotImplementedError
+        Heap.__init__(self, array)
 
     def extract_maximum(self):
-        pass
+        res = self.main[0]
+        last = len(self.main) - 1
+        self.main[0] = self.main[last]
+        self.main.pop()
+        i = 0
+        while 2*i+1 <= len(self.main) - 1:
+            # one leaf
+            if 2 * i + 1 == len(self.main) - 1:
+                j = 2*i + 1
+                if comparator_d(self.main[j], self.main[i]):
+                    # swap main[i] and main [j]
+                    h = self.main[i]
+                    self.main[i] = self.main[j]
+                    self.main[j] = h
+                break
+            else:
+                left_i = 2*i+1
+                right_i = 2*i + 2
+                if comparator_d(self.main[left_i], self.main[right_i]):
+                    j = left_i
+                else:
+                    j = right_i
+                if comparator_d(self.main[j], self.main[i]):
+                    # swap main[i] and main [j]
+                    h = self.main[i]
+                    self.main[i] = self.main[j]
+                    self.main[j] = h
+                i = j
+        return res
 
 
 def comparator_d(x, y):
--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -3,7 +3,7 @@
 
 
 from homeworks.homework_02.heap import MaxHeap
-from homeworks.homework_02.fastmerger import FastSortedListMerger
+# from homeworks.homework_02.fastmerger import FastSortedListMerger
 
 
 class VKPoster:
@@ -15,7 +15,7 @@ class VKPoster:
         # постами
         self.subs = {}
         # подписки
-        self.watches ={}
+        self.watches = {}
         # просмотры
         raise NotImplementedError
 
@@ -28,10 +28,12 @@ class VKPoster:
         :return: ничего
         '''
         if user_id not in self.userPost.keys():
-            self.userPost[user_id] = [post_id] #вводим в словарь юзера и пост по ключу и значению
+            self.userPost[user_id] = [post_id]
+            # вводим в словарь юзера и пост по ключу и значению
         elif post_id not in self.userPost(user_id):
-            self.userPost.get(user_id).append(post_id) #добавили новый пост к юзеру
-        if post_id not in  self.post.keys():
+            self.userPost.get(user_id).append(post_id)
+            # добавили новый пост к юзеру
+        if post_id not in self.post.keys():
             self.post[post_id] = [user_id, []]
 
     def user_read_post(self, user_id: int, post_id: int):
@@ -44,7 +46,7 @@ class VKPoster:
         '''
         if post_id not in self.post.keys():
             self.user_posted_post(-1, post_id)
-        if user_id not in self.posts.get(post_id)[1]:
+        if user_id not in self.post.get(post_id)[1]:
             self.post.get(post_id)[1].append(user_id)
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
@@ -56,7 +58,8 @@ class VKPoster:
         :return: ничего
         '''
         if follower_user_id not in self.sub.keys():
-            self.sub[follower_user_id] = [followee_user_id] #add to sub
+            self.sub[follower_user_id] = [followee_user_id]
+            # add to sub
         else:
             self.sub[follower_user_id].append(follower_user_id)
 
@@ -69,11 +72,13 @@ class VKPoster:
         :return: Список из post_id размером К из свежих постов в
         ленте пользователя. list
         '''
-        #lets use fastmerger
-        pass
-
-
-
+        # lets use fastmerger, but no, in future
+        post = list(self.post.keys())
+        post.sort(reverse=True)
+        recent_posts = [post_id for post_id in post
+                        if self.post.get(post_id)[0] in
+                        self.subs.get(user_id)]
+        return recent_posts[:k]
 
     def get_most_popular_posts(self, k: int) -> list:
         '''
@@ -83,4 +88,7 @@ class VKPoster:
         необходимо вывести. Число.
         :return: Список из post_id размером К из популярных постов. list
         '''
-        pass
+        # lets use MaxHeap
+        h = MaxHeap([(len(self.post.get(i)[1]), i)
+                     for i in list(self.post.keys())])
+        return [h.extract_maximum()[1] for i in range(k)]
