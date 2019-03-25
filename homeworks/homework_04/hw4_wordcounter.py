1b3fbc18a2f4c299f4c3fafabc73dea4d34ebcbe
orline09 <littlehappiness4@gmail.com>
2019-03-26 01:30:21 +0300

    try01

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing.dummy import Pool
+from functools import partial
 import os
 
 
 def word_count_inference(path_to_dir):
-    '''
+    """
     Метод, считающий количество слов в каждом файле из директории
     и суммарное количество слов.
     Слово - все, что угодно через пробел, пустая строка "" словом не считается,
@@ -15,5 +16,22 @@ def word_count_inference(path_to_dir):
     :param path_to_dir: путь до директории с файлами
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
-    '''
-    raise NotImplementedError
+    """
+    files = os.listdir(path_to_dir)
+    new_dict, new_dict['total'] = {}, 0
+    func = partial(count_dict, path_to_dir, new_dict)
+    pool = Pool(os.cpu_count())
+    pool.map(func, files)
+    return new_dict
+
+
+def reading_file(path_to_dir, filename):
+    with open(path_to_dir + '/' + filename, "r") as file:
+        return len(file.read().split())
+
+
+def count_dict(path_to_dir, dict_one, filename):
+    k = reading_file(path_to_dir, filename)
+    dict_one[filename] = k
+    dict_one['total'] += k
+    return
