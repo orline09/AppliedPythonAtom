666150260de116d376c7929b3533841cadcace60
orline09 <littlehappiness4@gmail.com>
2019-04-09 02:42:25 +0300

    check test simplex method

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,22 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    matrix = np.array([])
+    x = np.concatenate((a, [c * (-1)]), axis=0)
+    x = np.concatenate((x, np.eye(b.shape[0] + 1)), axis=1)
+    x = np.concatenate((x, np.append(b, [0], axis=0).reshape(x.shape[0], 1)), axis=1)
+
+    while x[-1, x[-1, :] < 0].shape[0] != 0:
+        pivot_column = np.argmin(x, axis=1)[-1]
+        row = np.argmin(x[:, -1][:-1] / x[:, pivot_column][:-1])
+        x[row, :] = x[row, :] / x[row, pivot_column]
+        for i in np.arange(x.shape[0]):
+            if i == row:
+                continue
+            x[i, :] = x[i, :] - x[row, :] * x[i, pivot_column]
+    for j in np.arange(a.shape[1]):
+        if (np.linalg.norm(x[:, j]) == 1) & (len(np.nonzero(x[:, j])[0]) == 1):
+            matrix = np.append(matrix, x[np.nonzero(x[:, j])[0], -1])
+        else:
+            matrix = np.append(matrix, [0])
+    return matrix
