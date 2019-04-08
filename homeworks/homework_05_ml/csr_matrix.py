39eb7c5a17bdcff56d7f687aa6441cdc7f1d2f06
orline09 <littlehappiness4@gmail.com>
2019-04-09 02:34:03 +0300

    try01 matrix

--- homeworks/homework_05_ml/csr_matrix.py
+++ homeworks/homework_05_ml/csr_matrix.py
@@ -10,39 +10,65 @@ class CSRMatrix:
     CSR (2D) matrix.
     Here you can read how CSR sparse matrix works: https://en.wikipedia.org/wiki/Sparse_matrix
     """
-    def __init__(self, init_matrix_representation):
+    def __init__(self, init_matrix):
         """
-        :param init_matrix_representation: can be usual dense matrix
+        :param init_matrix: can be usual dense matrix
         or
         (row_ind, col, data) tuple with np.arrays,
             where data, row_ind and col_ind satisfy the relationship:
             a[row_ind[k], col_ind[k]] = data[k]
         """
-        if isinstance(init_matrix_representation, tuple) and len(init_matrix_representation) == 3:
-            pass
-        elif isinstance(init_matrix_representation, np.ndarray):
-            pass
+        self.shape = init_matrix.shape
+        self.A = np.array([])
+        self.IA = np.array([0])
+        self.JA = np.array([])
+        if isinstance(init_matrix, tuple) and len(init_matrix) == 3:
+            self.A = np.append(self.A, init_matrix[2])
+            self.JA = np.append(self.JA, init_matrix[1])
+            self.IA = np.zeros(init_matrix[0][-1] + 2)
+            for i in init_matrix[0]:
+                self.IA[i + 1:] += 1
+        elif isinstance(init_matrix, np.ndarray):
+            self.A = np.append(self.A, init_matrix[np.nonzero(init_matrix)])
+            for i in np.arange(init_matrix.shape[0]):
+                self.JA = np.append(self.JA, np.nonzero(init_matrix[i, :]))
+            sum = 0
+            for i in np.arange(init_matrix.shape[0]):
+                sum += init_matrix[i, :][np.nonzero(init_matrix[i, :])].shape[0]
+                self.IA = np.append(self.IA, sum)
         else:
             raise ValueError
 
-        raise NotImplementedError
-
     def get_item(self, i, j):
-        """
-        Return value in i-th row and j-th column.
-        Be careful, i and j may have invalid values (-1 / bigger that matrix size / etc.).
-        """
-        raise NotImplementedError
+        for k in np.arange(self.IA[i], self.IA[i + 1]):
+            if self.JA[int(k)] == j:
+                return self.A[int(k)]
+        return 0
 
     def set_item(self, i, j, value):
-        """
-        Set the value to i-th row and j-th column.
-        Be careful, i and j may have invalid values (-1 / bigger that matrix size / etc.).
-        """
-        raise NotImplementedError
+        found = False
+        for k in np.arange(self.IA[i], self.IA[i + 1]):
+            if self.JA[k] == j:
+                self.A[k] = value
+                found = True
+                break
+        if not found:
+            if self.IA[i + 1] - self.IA[i] == 0:
+                self.A = np.insert(self.A, self.IA[i + 1], value)
+                self.JA = np.insert(self.JA, self.IA[i + 1], j)
+                self.IA[i + 1:] += 1
+            for k in np.arange(self.IA[i], self.IA[i + 1]):
+                if (j < self.JA[k]) | (k == np.arange(self.IA[i], self.IA[i + 1])[-1]):
+                    self.A = np.insert(self.A, k + 1, value)
+                    self.JA = np.insert(self.JA, k + 1, j)
+                    self.IA[i + 1:] += 1
+                    break
 
     def to_dense(self):
-        """
-        Return dense representation of matrix (2D np.array).
-        """
-        raise NotImplementedError
+        max_row_len = int(self.IA.shape[0] - 1)
+        max_col_len = int(np.amax(self.JA) + 1)
+        result = np.zeros([max_row_len, max_col_len])
+        for i in np.arange(max_row_len):
+            for j in np.arange(self.IA[int(i)], self.IA[int(i) + 1]):
+                result[int(i), int(self.JA[int(j)])] = self.A[int(j)]
+        return result
